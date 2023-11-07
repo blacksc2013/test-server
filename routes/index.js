70604592ba157538a38a02d03165d032b4d682da
blacksc2013 <66272961+blacksc2013@users.noreply.github.com>
2023-11-08 01:11:43 +0300

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TEST APP' });
+
+  const minutes = Math.floor(process.uptime() / 60);
+  res.render('index', { title: 'TEST APP' , time: minutes });
 });
 
 module.exports = router;
